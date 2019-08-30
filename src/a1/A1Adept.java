0101daa8f7b8a60656df3f3731ab64b8c6de68bd
icedwards <icedwards@152.23.125.254>
2019-08-30 14:28:46 -0400

    First attempt at setting up necessary input.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,5 +10,57 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		// Number of Items in the Store
+		int numItems = scan.nextInt();
+		
+		// Number of Costumers
+		int numCostum = scan.nextInt();
+	}
+
+	public static class itemsInStore {
+		Scanner scan = new Scanner(System.in);
+
+		
+		String name;
+		double price;
+		
+		public void brand () {
+			name = scan.next();
+		}
+		public void cost () {
+			price = scan.nextDouble();
+		}
+	}
+	
+	public static class Costumers {
+		Scanner scan = new Scanner(System.in);
+
+		String first;
+		String last;
+		int cart;
+		
+		public void firstName () {
+			first = scan.next();
+		}
+		public void lastName () {
+			last = scan.next();
+		}
+		public void numItemsPurchased () {
+			cart = scan.nextInt();
+		}
+	}
+	
+	public static class itemsInCart {
+		Scanner scan = new Scanner(System.in);
+		
+		int quantity;
+		String title;
+		
+		public void amount () {
+			quantity = scan.nextInt();
+		}
+		public void whatsItCalled () {
+			title = scan.next();
+		}
 	}
 }
