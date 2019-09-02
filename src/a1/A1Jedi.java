09861350b178765833a9dc396993b853e6dc7cc5
icedwards <icedwards@imanis-mbp>
2019-09-01 23:59:58 -0400

    Created instances for input

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,92 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		// Number of Items in the Store
+				int numItems = scan.nextInt();
+				
+				// What's in stock at the store?
+				// Array of the items available, the name and price of each
+				itemsInStore[] stock = new itemsInStore[numItems];
+				for (int i = 0; i < numItems; i++) {
+					stock[i] = new itemsInStore();
+					stock[i].name = scan.next();
+					stock[i].price = scan.nextDouble();
+				}
+				
+				// Number of Customers
+				int numCustomers = scan.nextInt();
+				
+				// Who's Shopping?
+				// Array of customers, their name and how many items they bought.
+				customers[] shoppers = new customers[numCustomers];
+				for (int i = 0; i < numCustomers; i++) {
+					shoppers[i] = new customers();
+					shoppers[i].first = scan.next();
+					shoppers[i].last = scan.next();
+					shoppers[i].numOfItemsBought = scan.nextInt(); 
+					
+					// What's in their cart?
+					// Array of the quantity and name of the items each costumer bought
+					itemsInCart[] cart = new itemsInCart[shoppers[i].numOfItemsBought];
+					for (int h = 0; h < shoppers[i].numOfItemsBought; h++) {
+						cart[h] = new itemsInCart();
+						cart[h].quantity = scan.nextInt();
+						cart[h].name = scan.next();
+					}
+				}
 		
 	}
+	
+	// Classes
+
+	// in stock at the store
+		public static class itemsInStore {
+			Scanner scan = new Scanner(System.in);
+			
+			String name;
+			double price;
+			
+			public void brand () {
+				name = scan.next();
+			}
+			public void cost () {
+				price = scan.nextDouble();
+			}
+		}
+	// customers shopping at the store
+		public static class customers {
+			Scanner scan = new Scanner(System.in);
+			
+			String first;
+			String last;
+			int numOfItemsBought;
+			
+			public void firstName () {
+				first = scan.next();
+			}
+			public void lastName () {
+				last = scan.next();
+			}
+			public void itemsPurchased () {
+				numOfItemsBought = scan.nextInt();
+			}
+		}
+	
+	// items in the carts of each customer
+		public static class itemsInCart {
+			Scanner scan = new Scanner(System.in);
+			
+			int quantity;
+			String name;
+			
+			public void amount () {
+				quantity = scan.nextInt();
+			}
+			public void itemName () {
+				name = scan.next();
+			}
+			
+		}
+		
+		
 }
